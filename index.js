c563470aaf2d80308c8d28043745783330cb4ba8
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-02-17 19:13:09 -0600

    .env added url for database connection but is only placeholder for now

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 
-mongoose.connect('mongodb://localhost:27017/test', { useNewUrlParser: true, useUnifiedTopology: true });
+dotenv.config();
+// need to add mongo url in .env file
+mongoose.connect(process.env.MONGO_URL).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(err => console.log(err));
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
