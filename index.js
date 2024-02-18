d3306c77af16640fbf31fa52b820c642db57f5ba
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-02-17 20:15:16 -0600

    set routes, and started auth routes for registering new user

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const userRoute = require('./routes/user');
+const authRoute = require('./routes/auth');
 
 dotenv.config();
 // need to add mongo url in .env file
@@ -11,6 +12,7 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
 }).catch(err => console.log(err));
 
 app.use(express.json());
+app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 3000, () => {
