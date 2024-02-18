71b97838d8e20cc4e880f4fb1061bf2d5ec1d1f4
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-02-17 19:22:14 -0600

    added user route, set up express router

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const userRoute = require('./routes/user');
 
 dotenv.config();
 // need to add mongo url in .env file
@@ -9,6 +10,8 @@ mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log('Connected to MongoDB');
 }).catch(err => console.log(err));
 
-app.listen(3000, () => {
+app.use("/api/users", userRoute);
+
+app.listen(process.env.PORT || 3000, () => {
     console.log('Server is running on port 3000');
 });
