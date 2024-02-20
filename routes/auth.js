3859eaa161ca646b712fb27fa26b5b8ca67c3075
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-02-19 18:20:10 -0600

    set up jwt and accessToken

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 // Register a new user
 router.post("/register", async (req, res) => {
@@ -26,11 +27,16 @@ router.post("/login", async (req, res) => {
         !user && res.status(400).json("Wrong username or password");
         
         const validPassword = await bcrypt.compare(user.password, process.env.PASSWORD);
+        
         const goodPassword = validPassword.toString(bcrypt.compare(user.password, process.env.PASSWORD));
         !goodPassword && res.status(400).json("Wrong username or password");
+        
+        const accessToken = jwt.sign({
+            id:user._id, isAdmin: user.isAdmin
+        }, process.env.JWT, {expiresIn: "5d"});
 
         const {password, ...others} = user._doc;
-        res.status(200).json(user);
+        res.status(200).json({ ...others, accessToken });
     }catch(err){
         res.status(500).json(err);
     }
