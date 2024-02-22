64fde4aaf253a2ca94efd6f00fbdf49f12371c19
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-02-21 19:44:39 -0600

    route to get product by its id

--- routes/product.js
+++ routes/product.js
@@ -47,7 +47,13 @@ router.delete("/:id", verifyTokenAdmin, async (req, res) => {
 
 // Get product by id
 router.get("/find/:id", async (req, res) => {
-    
+
+    try{
+        const product = await Product.findById(req.params.id);
+        res.status(200).json(product);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
 
