ccbf205179955810f295685e9b7f1f16acbc265d
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-02-21 20:20:47 -0600

    route to create a new cart

--- routes/cart.js
+++ routes/cart.js
@@ -1,4 +1,24 @@
+const Cart = require('../models/Cart');
+const {
+    verifyToken,
+    verifyTokenAuth,
+    verifyTokenAdmin,
+} = require('./verifyToken');
+
+
 const router = require('express').Router();
+
+// Create cart
+router.post('/', verifyToken, async (req, res) => {
+    const newCart = new Cart(req.body);
+
+    try {
+        const savedCart = await newCart.save();
+        res.status(200).json(savedCart);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
 
