5bb1c8d93247c713e1c159fd75bf46204d438a99
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-02-21 19:21:09 -0600

    route to create product

--- routes/product.js
+++ routes/product.js
@@ -1,4 +1,23 @@
+const Product = require("../models/Product");
+const {
+    verifyToken,
+    verifyTokenAuth,
+    verifyTokenAdmin,
+} = require("./verifyToken");
+
 const router = require('express').Router();
+
+// Create product
+router.post("/", verifyTokenAdmin, async (req, res) => {
+    const newProduct = new Product(req.body);
+
+    try {
+        const savedProduct = await newProduct.save();
+        res.status(200).json(savedProduct);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 
 
