839b104e113d28ed6f1d44006df4a2118ea870f5
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-02-21 20:03:25 -0600

    route to update order

--- routes/order.js
+++ routes/order.js
@@ -20,6 +20,20 @@ router.post('/', verifyToken, async (req, res) => {
     }
 });
 
+// Update order
+router.put('/:id', verifyTokenAdmin, async (req, res) => {
+    try {
+        const updatedOrder = await Order.findByIdAndUpdate(
+            req.params.id,
+            { $set: req.body },
+            { new: true }
+        );
+        res.status(200).json(updatedOrder);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
 
 
 
