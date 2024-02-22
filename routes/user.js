5d609347a57d8a2a12b42881d8ec0a90d5aaea1b
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-02-21 18:35:11 -0600

    added route to get user by id and comments to distinguish each route

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,8 @@
-const { verifyToken, verifyTokenAuth } = require('./verifyToken');
+const { verifyToken, verifyTokenAuth, verifyTokenAdmin } = require('./verifyToken');
 
 const router = require('express').Router();
 
+// Update user
 router.put("/:id", verifyTokenAuth, async (req, res) => {
     if(req.body.password) {
         req.body.password = bcrypt.hashSync(req.body.password, process.env.PASSWORD)
@@ -19,11 +20,25 @@ router.put("/:id", verifyTokenAuth, async (req, res) => {
     }
 });
 
+// Delete user by id
 router.delete("/:id", verifyTokenAuth, async (req, res) => {
     try{
         await User.findByIdAndDelete(req.params.id);
         res.status(200).json("User has been deleted successfully");
-    }catch(err){
+    } catch (err){
+        res.status(500).json(err);
+    }
+});
+
+// Get user by id
+router.get("/", verifyTokenAdmin, async (req, res) => {
+    const query = req.query.new;
+    try {
+        const users = query 
+        ? await User.find().sort({ _id: -1}).limit(5)
+        : await User.find();
+        res.status(200).json(users);
+    } catch (err) {
         res.status(500).json(err);
     }
 });
