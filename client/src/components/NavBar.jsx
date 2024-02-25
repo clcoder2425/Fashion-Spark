0f81db1068bba9a21681ed4bec255886bb67771f
clcoder2425 <141270027+clcoder2425@users.noreply.github.com>
2024-02-24 18:46:02 -0600

    modified corrected typo in NavBar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -31,7 +31,7 @@ function NavBar({ currentPage, handlePageChange }) {
       <li className="nav-item">
         <a 
           href="#Register"
-          onClick={() => handlePageChange('REgister')}
+          onClick={() => handlePageChange('Register')}
           // Check to see if the currentPage is `Contact`, and if so we use the active link class from bootstrap. Otherwise, we set it to a normal nav-link
           className={currentPage === 'Register' ? 'nav-link active' : 'nav-link'}
         >
