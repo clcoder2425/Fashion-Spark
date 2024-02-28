6fdb79bfcbbcc265b7dc27653bbe5afc56e90c3b
unknown <kar.alexander4325@gmail.com>
2024-02-28 17:24:44 -0600

    updated ReadMe and updated Sider at full screen, mobile view needs work

--- client/src/components/Slider.jsx
+++ client/src/components/Slider.jsx
@@ -33,9 +33,10 @@ z-index: 2;`
 
 const Wrapper = styled.div`
   height: 100%;
+
   display: flex;
   transition: all 1.5s ease;
-  transform: translateX(${(props) => props.slideIndex * -100}vw);
+  transform: translateX(${(props) => props.slideIndex * -70}vw);
 `;
 const Slide = styled.div`
 width:135vh;
