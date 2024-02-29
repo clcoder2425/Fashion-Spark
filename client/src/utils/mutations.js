8a93eaa85de98d23ec27eb32e64f0a30c536949b
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-02-28 21:42:49 -0600

    added login and add_order

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -11,3 +11,32 @@ export const ADD_USER = gql`
         }
     }
     `;
+
+export const LOGIN = gql`
+    mutation login($email: String!, $password: String!) {
+        login(email: $email, password: $password) {
+            token
+            user {
+                _id
+            }
+        }
+    }
+    `;
+
+    export const ADD_ORDER = gql`
+    mutation addOrder($products: [ID]!) {
+        addOrder(products: $products) {
+            purchaseDate
+            products {
+                _id
+                name
+                description
+                price
+                quantity
+                category {
+                    name
+                }
+            }
+        }
+    }
+    `;
