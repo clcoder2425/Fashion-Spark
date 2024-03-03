a181a1e74e62d90f1b0fd26ac9b1370110e9e389
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-03-02 21:05:36 -0600

    changed MONGO_URL to MONGO_URI

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL || "mongodb+srv://APIuser:ZoQdtJrHTRVoMFK7@cluster0.gtxvbu3.mongodb.net/Fashion-Spark-DB");
+mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://APIuser:ZoQdtJrHTRVoMFK7@cluster0.gtxvbu3.mongodb.net/Fashion-Spark-DB");
 
 module.exports = mongoose.connection;
