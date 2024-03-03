a837d118e4c057219aec8cca900a1321e32db444
Greg Pelkola <141973953+gregpelkola@users.noreply.github.com>
2024-03-02 21:10:00 -0600

    changed PORT env variable from 3000 to 3001 to deploy to render

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 // Integrating Apollo Server with Express
 server.applyMiddleware({ app });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
